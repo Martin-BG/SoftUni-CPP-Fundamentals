80c5f3201c7d95ef240a9852c845df689ce5fb87
MartinBG <MartinBG@abv.bg>
2019-11-03 04:59:10 +0200

    JA3 task #2

---
+++ 06. Classes and Objects/JA3-2018.07.7/02. Copy-Paste.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+#include <sstream>
+
+class TextEditor {
+private:
+  std::stack<std::string> clipboard{ };
+  std::string text;
+
+  void copy(size_t start, size_t end) {
+    while (start > 0 && this->text[start - 1] != ' ') {
+      --start;
+    }
+
+    end = this->text.find_first_of(' ', end);
+
+    const size_t len = end - start;
+
+    std::string copied = this->text.substr(start, len);
+
+    clipboard.push(copied);
+  }
+
+  void paste(size_t index) {
+    if (!clipboard.empty()) {
+      std::string toPaste = clipboard.top();
+      clipboard.pop();
+
+      if (this->text[index] == ' ') {
+        toPaste.insert(toPaste.begin(), ' ');
+      }
+
+      this->text.insert(index, toPaste);
+    }
+  }
+
+public:
+  explicit TextEditor(std::string text) : text(std::move(text)) { }
+
+  void handleInput(const std::string& input) {
+    std::istringstream iss(input);
+    std::string command;
+    iss >> command;
+    if (command == "copy") {
+      size_t start, end;
+      iss >> start >> end;
+      copy(start, end);
+    } else if (command == "paste") {
+      size_t index;
+      iss >> index;
+      paste(index);
+    }
+  }
+
+  const std::string& getText() const {
+    return text;
+  }
+};
+
+int main() {
+  std::istream& in = std::cin;
+  std::ostream& out = std::cout;
+
+  std::string text;
+  std::getline(in, text);
+
+  TextEditor textEditor{ text };
+
+  std::string command;
+  while (std::getline(in, command) && command != "end") {
+    textEditor.handleInput(command);
+  }
+
+  out << textEditor.getText();
+
+  return 0;
+}
