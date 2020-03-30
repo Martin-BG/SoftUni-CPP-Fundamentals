2010a4107787acc3232b16b59c98049820defe2f
MartinBG <MartinBG@abv.bg>
2020-03-30 20:13:48 +0300

    Task #3

---
+++ 02. Functions, Arrays and Vectors/Judge/03. Above Average.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+void readArrayFromConsole(int*& arr, int& length);
+
+int calculateAverage(const int* arr, int length);
+
+int main() {
+  int length;
+  int* arr;
+
+  readArrayFromConsole(arr, length);
+  int average = calculateAverage(arr, length);
+
+  for (int i = 0; i < length; ++i) {
+    if (arr[i] >= average) {
+      std::cout << arr[i] << " ";
+    }
+  }
+  std::cout << std::endl;
+
+  delete[] arr;
+
+  return 0;
+}
+
+int calculateAverage(const int* arr, int length) {
+  long sum{ 0L };
+  for (int i = 0; i < length; ++i) {
+    sum += arr[i];
+  }
+
+  return sum / length;
+}
+
+void readArrayFromConsole(int*& arr, int& length) {
+  std::cin >> length;
+  arr = new int[length];
+
+  for (int number, i = 0; i < length; ++i) {
+    std::cin >> number;
+    arr[i] = number;
+  }
+}
