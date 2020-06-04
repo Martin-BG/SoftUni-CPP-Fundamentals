230e9e168ee13d6c1895d2b49b917b9f08cc3063
MartinBG <MartinBG@abv.bg>
2020-06-04 22:21:14 +0300

    task #1

--- 07. Exams/2019.02.03/01. Min-Max Temperature.cpp
+++ 07. Exams/2019.02.03/01. Min-Max Temperature.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <map>
 
 int main() {
-  // TODO
+  std::map<std::string, std::pair<double, double>> towsMinMaxTemperatures{ };
+
+  size_t towns;
+  std::cin >> towns;
+
+  while (towsMinMaxTemperatures.size() < towns) {
+    double min, max;
+    std::string town;
+    std::cin >> town >> min >> max;
+
+    if (towsMinMaxTemperatures.find(town) == towsMinMaxTemperatures.end()) {
+      towsMinMaxTemperatures.emplace(town, std::make_pair(min, max));
+    }
+
+    if (towsMinMaxTemperatures[town].first > min) {
+      towsMinMaxTemperatures[town].first = min;
+    }
+
+    if (towsMinMaxTemperatures[town].second < max) {
+      towsMinMaxTemperatures[town].second = max;
+    }
+  }
+
+  for (const auto& townTemps: towsMinMaxTemperatures) {
+    const std::string& town = townTemps.first;
+    const double minTemp = townTemps.second.first;
+    const double maxTemp = townTemps.second.second;
+    std::cout << town << " " << minTemp << " " << maxTemp << std::endl;
+  }
 
   return 0;
 }
-
