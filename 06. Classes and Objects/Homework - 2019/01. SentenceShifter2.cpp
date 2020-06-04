97288c28c956e868fbf29d0e88583895a26d1b72
MartinBG <MartinBG@abv.bg>
2020-06-04 04:47:18 +0300

    alternative solution for Sentence Shifter problem

---
+++ 06. Classes and Objects/Homework - 2019/01. SentenceShifter2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+class WordsShifter {
+  const std::vector<std::string> words;
+public:
+  explicit WordsShifter(std::vector<std::string> words) : words(std::move(words)) { }
+
+  std::vector<std::string> getShiftedBy(size_t shiftBy) {
+    std::vector<std::string> shiftedWords{ this->words.size() };
+
+    shiftBy %= this->words.size();
+
+    std::rotate_copy(
+        this->words.rbegin(),
+        this->words.rbegin() + shiftBy,
+        this->words.rend(),
+        shiftedWords.rbegin());
+
+    return shiftedWords;
+  }
+};
+
+void printVectorToConsole(std::vector<std::string> shiftedWords);
+
+std::vector<std::string> parseWordsFromConsole();
+
+int main() {
+  WordsShifter words(parseWordsFromConsole());
+
+  size_t shiftBy;
+  std::cin >> shiftBy;
+
+  printVectorToConsole(words.getShiftedBy(shiftBy));
+
+  return 0;
+}
+
+void printVectorToConsole(std::vector<std::string> shiftedWords) {
+  std::ostringstream oss;
+  std::move(shiftedWords.begin(), shiftedWords.end(), std::ostream_iterator<std::string>(oss, "\n"));
+  std::cout << oss.str();
+}
+
+std::vector<std::string> parseWordsFromConsole() {
+  std::string line;
+  std::getline(std::cin, line);
+
+  std::istringstream iss(line);
+
+  std::vector<std::string> words{
+      std::istream_iterator<std::string>{ iss },
+      std::istream_iterator<std::string>{ }};
+
+  return words;
+}
