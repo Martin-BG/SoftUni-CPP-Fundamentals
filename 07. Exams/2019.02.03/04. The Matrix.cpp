0ca94e7874e5dfc4f4026211836e5e3853f753e5
MartinBG <MartinBG@abv.bg>
2020-06-05 05:45:36 +0300

    task #4

--- 07. Exams/2019.02.03/04. The Matrix.cpp
+++ 07. Exams/2019.02.03/04. The Matrix.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <vector>
+
+bool isSimpleNumber(int number) {
+  if (number != 2) {
+    if (number < 1 || number % 2 == 0) {
+      return false;
+    }
+
+    for (int i = 3; (i * i) <= number; i += 2) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 int main() {
-  // TODO
+  size_t matrixSize;
+  std::cin >> matrixSize;
+
+  std::vector<std::vector<int>> matrix{ matrixSize, std::vector<int>(matrixSize) };
+
+  for (size_t row = 0; row < matrixSize; ++row) {
+    for (size_t col = 0; col < matrixSize; ++col) {
+      std::cin >> matrix[row][col];
+    }
+  }
+
+  long long sum = 0;
+  if (matrixSize > 2) {
+    for (size_t row = matrixSize - 1, offset = 1; row > matrixSize / 2; --row, ++offset) {
+      for (size_t col = offset; col < matrixSize - offset; ++col) {
+        if (isSimpleNumber(matrix[row][col])) {
+          sum += matrix[row][col];
+        }
+      }
+    }
+  }
+  std::cout << sum;
 
   return 0;
 }
-
