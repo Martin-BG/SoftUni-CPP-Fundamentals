369a454b47e746c1e235499b5bc7a6bd12a997d2
MartinBG <MartinBG@abv.bg>
2020-09-30 02:21:02 +0300

    refactored

--- 02. Functions, Arrays and Vectors/Judge/02. Longest Sequence.cpp
+++ 02. Functions, Arrays and Vectors/Judge/02. Longest Sequence.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-void readArrayFromConsole(int*& arr, int& length);
+int* readArrayFromConsole(int length);
 
 void findLongestSequence(const int* arr, int length, int& index, int& lengthMax);
 
 int main() {
   int length;
-  int* arr;
-
-  readArrayFromConsole(arr, length);
+  std::cin >> length;
+  const int* arr = readArrayFromConsole(length);
 
   int longestStartIndex, longestLength;
   findLongestSequence(arr, length, longestStartIndex, longestLength);
@@ -23,30 +22,28 @@ int main() {
   return 0;
 }
 
-void findLongestSequence(const int* arr, int length, int& index, int& lengthMax) {
+void findLongestSequence(const int* arr, const int length, int& index, int& lengthMax) {
   index = 0;
   lengthMax = 1;
 
-  for (int lengthCurrent = 1, prev = arr[0], i = 1; i < length; ++i) {
+  for (int lengthCurrent = 1, prev = arr[0], i = 1; i < length; prev = arr[i], ++i) {
     if (prev != arr[i]) {
       lengthCurrent = 0;
     }
 
     if (++lengthCurrent >= lengthMax) {
       lengthMax = lengthCurrent;
-      index = i - lengthMax + 1;
+      index = i - lengthCurrent + 1;
     }
-
-    prev = arr[i];
   }
 }
 
-void readArrayFromConsole(int*& arr, int& length) {
-  std::cin >> length;
-  arr = new int[length];
+int* readArrayFromConsole(const int length) {
+  int* arr = new int[length];
 
-  for (int number, i = 0; i < length; ++i) {
-    std::cin >> number;
-    arr[i] = number;
+  for (int i = 0; i < length; ++i) {
+    std::cin >> arr[i];
   }
+
+  return arr;
 }
