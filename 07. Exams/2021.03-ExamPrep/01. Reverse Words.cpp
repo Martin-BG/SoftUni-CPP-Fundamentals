03560b3d970ddd52a14deb121324999cf8ae1ce6
MartinBG <MartinBG@abv.bg>
2021-06-01 01:27:57 +0300

    solve 2021.03 exam prep

--- 07. Exams/2021.03-ExamPrep/01. Reverse Words.cpp
+++ 07. Exams/2021.03-ExamPrep/01. Reverse Words.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
 
 int main() {
-  // TODO
+  std::vector<std::string> words{ };
+
+  std::string line;
+  while (std::getline(std::cin, line) && line != "end") {
+    std::istringstream ss(line);
+    std::string word;
+    while (ss >> word) {
+      words.emplace_back(word);
+    }
+  }
+
+  std::copy(words.crbegin(), words.crend(), std::ostream_iterator<std::string>(std::cout, " "));
 
   return 0;
 }
--- 07. Exams/2021.03-ExamPrep/02. Numeral System.cpp
+++ 07. Exams/2021.03-ExamPrep/02. Numeral System.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int decode(const std::vector<char>& digits, const std::vector<char>& encoded);
+
+std::string encode(const std::vector<char>& digits, int number);
 
 int main() {
-  // TODO
+  std::vector<char> digits(10);
+  std::copy_n(std::istream_iterator<char>(std::cin >> std::ws), 10, digits.begin());
+
+  std::string input;
+  std::getline(std::cin >> std::ws, input);
+  const std::vector<char> first(input.cbegin(), input.cend());
+
+  std::getline(std::cin >> std::ws, input);
+  const std::vector<char> second(input.cbegin(), input.cend());
+
+  const int firstNum = decode(digits, first);
+  const int secondNum = decode(digits, second);
+  const int sum = firstNum + secondNum;
+
+  std::string encoded = encode(digits, sum);
+
+  std::cout << encoded;
 
   return 0;
+}
+
+std::string encode(const std::vector<char>& digits, const int number) {
+  std::string encoded{ };
+  for (char ch : std::to_string(number)) {
+    encoded += digits[ch - '0'];
+  }
+  return encoded;
+}
+
+int decode(const std::vector<char>& digits, const std::vector<char>& encoded) {
+  int decoded = 0;
+  for (char ch : encoded) {
+    decoded *= 10;
+    int value = (int) std::distance(digits.cbegin(), std::find(digits.cbegin(), digits.cend(), ch));
+    decoded += value;
+  }
+  return decoded;
 }
